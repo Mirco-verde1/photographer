a606e792887c46247bd80b2e334c078c14f8315c
Mirco Verderosa <verderosa@medialogic.it>
2022-08-22 14:02:02 +0200

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    @include('page_elements.navbar')
+    <h2 class="nav">Mirco</h2>
+    <div class="box">
+        <img src="{{asset('images/test-image.jpeg')}}" alt="" srcset="">
+    </div>
+</body>
+</html>
---
+++ resources/views/page_elements/navbar.blade.php
@@ -0,0 +1,3 @@
+<div class="navbar-container col-lg-12">
+NAVBAR PRINCIPALE
+</div>
